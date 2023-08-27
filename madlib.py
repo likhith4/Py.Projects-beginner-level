c0f5b3af3ed7cba9f665974bdd00d392c0e2a1a2
likhith <likhithk495@gmail.com>
2023-08-27 19:27:03 +0530

    finished

--- madlib.py
+++ madlib.py
@@ -4,7 +4,7 @@ words=set() #using set to get only unique words which will cut off all the dupli
 start_of_word=-1
 target_start="<"
 target_end=">"
-for i,char in enumerate(story):
+for i,char in enumerate(story): #enumerate gives the index val and char
     if char == target_start:
         start_of_word=i
     
@@ -12,5 +12,11 @@ for i,char in enumerate(story):
         word=story[start_of_word:i+1] #string slicing will not include start of word i+1 will include the char at i th value
         words.add(word)
         start_of_word=-1
-print(words)
+answers={}
+for word in words:
+    ans=input("Enter a word for "+word+":")
+    answers[word]=ans
+for word in words:
+    story=story.replace(word,answers[word])#.replace replaces the words 
+print(story)
 
